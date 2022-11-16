c2587b3ded5dbe8a8e6df50b26e70ee3634d7986
anami <anami@La-mamalona-de-Ana>
2022-11-16 14:22:20 -0700

    a veeeeerr

---
+++ src/main/java/com/mycompany/giypinchehub/Giypinchehub.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.giypinchehub;
+
+/**
+ *
+ * @author anami
+ */
+public class Giypinchehub {
+
+    public static void main(String[] args) {
+        System.out.println("Hoy es jueves");
+    }
+}
